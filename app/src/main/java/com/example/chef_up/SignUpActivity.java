26de88dc0248adfc7d14580d41bc0844a6c758ee
pharsh2525 <p.harsh2525@gmail.com>
2023-11-16 01:58:54 -0600

    Added Signup screen with validation, fixed activity stack

--- app/src/main/java/com/example/chef_up/SignUpActivity.java
+++ app/src/main/java/com/example/chef_up/SignUpActivity.java
@@ -9,47 +9,54 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
-public class LoginActivity extends AppCompatActivity {
-
-
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
-
-        Button loginButton = findViewById(R.id.btnLogin);
         Button signUpButton = findViewById(R.id.btnSignUp);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(login()){
-                    Intent logInIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(logInIntent);
-                    finish();
-                }
-            }
-        });
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signUpIntent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(signUpIntent);
+                if(signUp()){
+                    Intent signUpIntent = new Intent(SignUpActivity.this, MainActivity.class);
+                    signUpIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(signUpIntent);
+                    finish();
+                }
             }
         });
     }
 
-    private boolean login() {
+    private boolean signUp() {
+        TextInputEditText firstNameInput = findViewById(R.id.inFirstName);
+        TextInputEditText lastNameInput = findViewById(R.id.inLastName);
         TextInputEditText emailInput = findViewById(R.id.inEmail);
         TextInputEditText passwordInput = findViewById(R.id.inPassword);
         boolean isValid = true;
 
+        String firstName = firstNameInput.getText().toString().trim();
+        String lastName = lastNameInput.getText().toString().trim();
         String email = emailInput.getText().toString().trim();
         String password = passwordInput.getText().toString().trim();
+
+        if(firstName.isEmpty()){
+            firstNameInput.setError("First name is required");
+            isValid = false;
+        } else{
+            firstNameInput.setError(null);
+        }
+
+        if(lastName.isEmpty()){
+            lastNameInput.setError("Last name is required");
+            isValid = false;
+        } else{
+            lastNameInput.setError(null);
+        }
 
         if(email.isEmpty()){
             emailInput.setError("Email is required");
@@ -64,8 +71,9 @@ public class LoginActivity extends AppCompatActivity {
         if(password.isEmpty()) {
             passwordInput.setError("Password is required");
             isValid = false;
-        }
-        else{
+        } else if (password.length() < 8) {
+            passwordInput.setError("Password needs to be at least 8 characters");
+        } else{
             passwordInput.setError(null);
         }
 
--- app/src/main/java/com/example/chef_up/SplashActivity.java
+++ app/src/main/java/com/example/chef_up/SplashActivity.java
@@ -18,6 +18,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(iHome);
+                finish();
             }
         },3000);
 
